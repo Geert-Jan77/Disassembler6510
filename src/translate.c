df092057e1205bfdaa66ae0f9d0b564b952cc318
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2024-10-28 20:41:11 +0100

    Add files via upload

---
+++ src/translate.c
@@ -0,0 +1,101 @@
+//char value = *(char*)0x01f4b274; 
+//CHAR * pointer = (CHAR*)0x01f4b274; 
+#include <stdio.h>
+#include <Windows.h>
+
+
+int main(void)
+{
+	HMODULE libHandle;
+	BOOL bRet;
+	UINT uFreq, uDuration;
+	int* address = (int *)0x01f4b274; 
+	printf("Memory address is: 0x%x\n", address);
+	if ((libHandle = LoadLibrary(TEXT("C:/Windows/System32/kernel32.dll"))) == NULL)
+    {
+        printf("load failed\n");
+        return 1;
+    }
+	
+	FARPROC beep;
+    if ((beep = GetProcAddress(libHandle, "Beep")) == NULL)
+    {
+        printf("GetProcAddress failed\n");
+        printf("%d\n", GetLastError());
+        return 1;
+    }
+	printf("beep handle %d\n", beep);
+	uFreq = 1000;
+	uDuration = 200;
+	if (!(bRet = beep(uFreq, uDuration)))
+	{
+		printf("Beep failed\n");
+		return 1;
+	}
+
+	FARPROC openprocess;
+    if ((openprocess = GetProcAddress(libHandle, "OpenProcess")) == NULL)
+    {
+        printf("GetProcAddress failed\n");
+        printf("%d\n", GetLastError());
+        return 1;
+    }
+	printf("openprocess handle %d\n", openprocess);
+	UINT uFlags = 0x00000010;
+	BOOL bInherit = 0;
+	UINT uProcess = 0x18c0;
+	INT_PTR hProcess;
+//	if (!(hProcess = openprocess(uFlags, bInherit, uProcess)))
+//	{
+//		printf("OpenProcess failed\n");
+//		return 1;
+//	}
+//	printf("Game Process handle %d\n", hProcess);
+	SIZE_T written;
+	FARPROC ReadProcessMemory;
+    INT_PTR lpBaseAddress;
+    INT_PTR lpBuffer;
+    UINT dwSize;
+    INT_PTR lpNumberOfBytesRead;
+	lpBaseAddress = 0x01f4b274;
+	FLOAT f = 0.0f;
+	lpBuffer = (INT_PTR)&f;
+	dwSize = 4;
+	CHAR buffer[20];
+	lpNumberOfBytesRead = 0;
+	if ((ReadProcessMemory = GetProcAddress(libHandle, "ReadProcessMemory")) == NULL)
+    {
+        printf("GetProcAddress failed\n");
+        printf("%d\n", GetLastError());
+        return 1;
+    }
+	printf("ReadProcessMemory handle %d\n", ReadProcessMemory);
+//	if (!(bRet = ReadProcessMemory(hProcess, lpBaseAddress, &buffer, sizeof(f), lpNumberOfBytesRead)))
+//	{
+//		printf("ReadProcessMemory failed\n");
+//		return 1;
+//	}
+//	printf("Buffer0 %d\n", (byte)buffer[0]);
+//	printf("Buffer1 %d\n", (byte)buffer[1]);
+//	printf("Buffer2 %d\n", (byte)buffer[2]);
+//	printf("Buffer3 %d\n", (byte)buffer[3]);
+ 	byte b[] = {(byte)buffer[0], (byte)buffer[1], (byte)buffer[2], (byte)buffer[3]};
+	memcpy(&f, &b, sizeof(f));
+//	printf("f %f\n", f);
+	FARPROC CloseHandle;
+	if ((CloseHandle = GetProcAddress(libHandle, "CloseHandle")) == NULL)
+    {
+        printf("GetProcAddress failed\n");
+        printf("%d\n", GetLastError());
+        return 1;
+    }
+	printf("CloseProcess handle %d\n", CloseHandle);
+//	if (!(bRet = CloseHandle(hProcess)))
+//	{
+//		printf("CloseHandle failed\n");
+//		return 1;
+//	}
+	
+
+  return 0;
+}
