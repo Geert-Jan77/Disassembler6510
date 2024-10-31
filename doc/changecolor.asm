7ee8b79b5ceec694009ce99c28082ec154d870ce
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2024-10-31 18:10:16 +0100

    Add files via upload

---
+++ doc/changecolor.asm
@@ -0,0 +1,20 @@
+
+Disassembler6510 has decompiled the code into:
+
+adr   mne mde oprnd mnemonic description                  mode description 
+00000 .dt      2049 
+02049 .dt      2059 Point to next line
+02051 .dt        10 Basic line number
+02053 .by       158 Basic token 'sys'
+02054 .by        50 '2'
+02055 .by        48 '0'
+02056 .by        54 '6'
+02057 .by        49 '1'
+02058 .by         0 Line Break
+02059 .dt         0 End of Basic list
+02061 lda imm    12 Load Accumulator                      Immediate    
+02063 sta abs 53280 Store Accumulator                     Absolute     
+02066 rts           Return to Saved                       Implied      
+feof() end of file at 2066
+
+Found 5 ascii characters, use "dat" for dataview.
